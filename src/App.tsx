a499f74459e73cb0ddd2122763dc4c948973d6e2
Stephen Wahome <stephen.wahome@griffinglobaltech.com>
2025-01-15 18:24:29 +0300

    Added an emoji

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Gitflow playground -- JRS18</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Your count was {count} and is now {count + 1}
+          Your count was {count} and is now {count + 1} 😁😁😁
         </button>
 
         <p>Hello Git. 🙌 Creating a merge conflict😁</p>
